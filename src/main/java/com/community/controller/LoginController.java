efdb3396162ff265ec856d5f3190bad20e9d7eae
jacka <758168660@qq.com>
2021-11-02 17:11:43 +0800

    kaptcha

--- src/main/java/com/community/controller/LoginController.java
+++ src/main/java/com/community/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.community.controller;
 import com.community.entity.User;
 import com.community.service.UserService;
 import com.community.util.CommunityConstant;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String register() {
@@ -63,6 +77,26 @@ public class LoginController implements CommunityConstant {
         }
 
         return "/site/operate-result";
+    }
+
+
+    // 验证码
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        // 将验证码存入session中，方便后面使用
+        session.setAttribute("kaptcha", text);
+
+        // 将图片输出在浏览器
+        response.setContentType("image/png");
+        try {
+            ServletOutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("kaptcha error");
+        }
     }
 
 }
