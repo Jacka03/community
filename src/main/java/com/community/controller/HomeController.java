5abffac08df8793dafb5807bc3040fa3dfca2f60
Jacka03 <758168660@qq.com>
2021-12-27 22:58:00 +0800

    fix some error

--- src/main/java/com/community/controller/HomeController.java
+++ src/main/java/com/community/controller/HomeController.java
@@ -37,7 +37,7 @@ public class HomeController implements CommunityConstant {
         // 方法调用钱,SpringMVC会自动实例化Model和Page,并将Page注入Model.
         // 所以,在thymeleaf中可以直接访问Page对象中的数据.
         page.setRows(discussPostService.findDiscussPostRows(0));
-        page.setPath("/index?orderMode=orderMode");
+        page.setPath("/index?orderMode=" + orderMode);
 
         List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit(), orderMode);
         List<Map<String, Object>> discussPosts = new ArrayList<>();
